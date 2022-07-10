8502c9891811975f316b8cd29a166973b9141dad
SepYoKim6745 <108175712+SepYoKim6745@users.noreply.github.com>
2022-07-10 14:11:45 +0000

    6056-6068 comppleted

---
+++ Algorithm/Basics/6066.py
@@ -0,0 +1,15 @@
+a, b, c = input().split()
+if int(a)%2!=0 :
+  print('odd')
+else :
+  print('even')
+
+if int(b)%2!=0 :
+  print('odd')
+else :
+  print('even')
+
+if int(c)%2!=0 :
+  print('odd')
+else :
+  print('even')
---
+++ Algorithm/Basics/6068.py
@@ -0,0 +1,10 @@
+g = int(input())
+
+if g>=90 :
+  print('A')
+elif g>=70 :
+  print('B')
+elif g>=40 :
+  print('C')
+else :
+  print('D')
